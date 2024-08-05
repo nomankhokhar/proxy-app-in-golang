99070dbe59138ce6e2202a30dcd7ef0eead8f034
nomanali <aliyounas084@gmail.com>
2024-08-05 16:39:41 +0500

    testing api, allocation api, allocation params api

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 
 	router.GET("/ping", pingHandler)
 	router.GET("/allocation", allocationHandler)
+	router.GET("/allocation/:param1/:param2", paramAllocationHandler)
 
 	router.Run(":8080")
 }
@@ -46,4 +47,39 @@ func allocationHandler(c *gin.Context) {
 	}
 
 	c.JSON(resp.StatusCode(), result)
+}
+
+func paramAllocationHandler(c *gin.Context) {
+	param1 := c.Param("param1")
+	param2 := c.Param("param2")
+	param3 := c.Param("param3")
+
+	client := resty.New()
+
+	resp, err := client.R().
+		SetQueryParam("param1", param1).
+		SetQueryParam("param2", param2).
+		SetQueryParam("param3", param3).
+		Get("http://localhost:3000/allocation/summary")
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status": "error",
+			"error":  "Failed to fetch allocation summary",
+		})
+
+		return
+	}
+
+	var result map[string]interface{}
+
+	if err := json.Unmarshal(resp.Body(), &result); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to perase allocation summary response",
+		})
+		return
+	}
+
+	c.JSON(resp.StatusCode(), result)
+
 }
